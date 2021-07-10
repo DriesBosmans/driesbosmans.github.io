e4d45691522738e3e01bc608f6dc45fe6a20557f
DriesBosmans <73179981+DriesBosmans@users.noreply.github.com>
2021-07-11 00:35:42 +0200

    Cv als download

--- cv.js
+++ cv.js
@@ -1,5 +1,6 @@
 document.querySelector("#fb").style.transition = "all 0.2s";
 document.querySelector("#linkedin").style.transition = "all 0.2s";
+document.querySelector("#dwnld").style.transition = "all 0.2s";
 let navElements = document.querySelectorAll('.nav-item');
 for (let i = 0; i < navElements.length; i++) {
     navElements[i].style.transition = "all 0.2s";
