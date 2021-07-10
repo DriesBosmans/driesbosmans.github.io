899b8e79459dec1adc0b05e2c1af5bbf3d061d7a
DriesBosmans <73179981+DriesBosmans@users.noreply.github.com>
2021-07-10 23:43:39 +0200

    Kleine aanpassingen: style transitions

--- cv.js
+++ cv.js
@@ -1,2 +1,6 @@
-document.querySelector("#fb").style.transition = "all 0.1s";
-document.querySelector("#linkedin").style.transition = "all 0.1s";
+document.querySelector("#fb").style.transition = "all 0.2s";
+document.querySelector("#linkedin").style.transition = "all 0.2s";
+let navElements = document.querySelectorAll('.nav-item');
+for (let i = 0; i < navElements.length; i++) {
+    navElements[i].style.transition = "all 0.2s";
+}
